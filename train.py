e1d7d1337277ad5d819ddcb555dea0323e6e11a6
uthree <33905956+uthree@users.noreply.github.com>
2021-12-26 01:26:30 +0900

    change parameters

--- train.py
+++ train.py
@@ -19,4 +19,4 @@ dataset = ImageDataset(source_dir_pathes=[
                        max_len=100000,
 )
 
-trainer.train(dataset=dataset, initial_batch_size=32, num_epochs_per_resolution=50, max_resolution=1024, learning_rate=1e-4, save_path=model_path, results_dir_path='results/')
+trainer.train(dataset=dataset, initial_batch_size=32, num_epochs_per_resolution=100, max_resolution=1024, learning_rate=1e-4, save_path=model_path, results_dir_path='results/')
