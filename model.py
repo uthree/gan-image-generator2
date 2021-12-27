6c8a956389cbc1d5101e2196473bd30de78f2cf7
uthree <33905956+uthree@users.noreply.github.com>
2021-12-28 03:19:16 +0900

    fix

--- model.py
+++ model.py
@@ -86,8 +86,10 @@ class ToRGB(nn.Module):
     def __init__(self, input_channels):
         super(ToRGB, self).__init__()
         self.conv = nn.Conv2d(input_channels, 3, kernel_size=1, stride=1, padding=0)
+        self.tanh = nn.Tanh()
     def forward(self, x):
         x = self.conv(x)
+        x = self.tanh(x)
         return x
 
 class GeneratorBlock(nn.Module):
